f4f376d4f3d5d5410f59b75adf34b3d9e6304c43
pschwendy <peterschwende@gmail.com>
2020-12-31 12:53:27 -0500

    templated Disney’s Coronado Springs Resort part of Hotels page

--- huron/src/components/pages/Hotels.js
+++ huron/src/components/pages/Hotels.js
@@ -11,6 +11,8 @@ import {
   Carousel,
   CarouselItem,
   Card,
+  ListGroup,
+  ListGroupItem,
   CardColumns,
   CardGroup,
   CardDeck,
@@ -75,7 +77,7 @@ class Hotels extends Component{
                             <button className="btn no-click">
                                 Hotel: 
                             </button>
-                            <button id="yacht" className={this.state.yacht === this.HOTEL.YACHT ? "btn-primary btn ml-n2" : "btn ml-n2"} onClick={() => this.switchHotelsToYacht()}>
+                            <button id="yacht" className={this.state.yacht === this.HOTEL.YACHT ? "btn-primary btn" : "btn"} onClick={() => this.switchHotelsToYacht()}>
                                 Yacht Club
                             </button>
                             <button className={this.state.yacht === this.HOTEL.SPRINGS ? "btn-primary btn ml-2" : "btn ml-2"} onClick={() => this.switchHotelsToSprings()}>
@@ -84,9 +86,154 @@ class Hotels extends Component{
                         </div>
                     </div>
                     {
-                        this.state.yacht === this.HOTEL.YACHT?
+                        this.state.yacht === this.HOTEL.SPRINGS?
                         <div>
-                            HELLO
+                            <h2 className="heading">Disney’s Coronado Springs Resort</h2>
+                            <hr className="the-hr"></hr>
+                            <div class="row mt-3 article">
+                                <div class="col MR-3">
+                                    <div class="right-div">
+                                        <p class="left-justified">A captivating hotel with Spanish, Mexican, and Southwest American inspired architecture, built around the 15-acre glistening lake Lago Dorado, Disney offers a modern and energizing experience for everyone who chooses this as their destination home. With amenities such as an expansive Mayan-themed pool complex, 18 restaurants nearby the resort, and many more, Coronado Springs Resort will provide unique and unforgettable memories.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <img className="img-fluid article-image" src="https://d3bo0mk5kk7egf.cloudfront.net/imgstore/ElementGalleryItems/other/Fullsize/Maps_Full_30016.jpg?width=2400&quality=80&encoder=freeimage&progressive=true"></img>
+                            <hr></hr>
+                            <div class="alt-1"> 
+                                <div className="row article">
+                                    <div class="col-md mr-3">
+                                        <div class="right-div">
+                                            <section>
+                                                <p>General Amenities/Information:</p>
+                                                <ul className="mt-n3" aria-label="">
+                                                    <li>
+                                                        Built 08/01/1997
+                                                    </li>
+                                                    <li>
+                                                        1915 total number of room and suites in three villages around the lake, 545 rooms in 15-story Gran Destino Tower
+                                                    </li>
+                                                    <li>
+                                                        ADA compliant rooms: 99 
+                                                    </li>
+                                                    <li>
+                                                        WIFI
+                                                    </li>
+                                                    <li>
+                                                        Fitness Center 
+                                                    </li>
+                                                    <li>
+                                                        Free airport transportation
+                                                    </li>
+                                                    <li>
+                                                        24-hour security
+                                                    </li>
+                                                    <li>
+                                                        24-hour front desk
+                                                    </li>
+                                                    <li>
+                                                        Free shuttle / taxi services 
+                                                    </li>
+                                                    <li>
+                                                        Outdoor Pool 
+                                                    </li>
+                                                    <li>
+                                                        Heated Pool
+                                                    </li>
+                                                    <li>
+                                                        Baggage Storage
+                                                    </li>
+                                                    <li>
+                                                        Spa
+                                                    </li>
+                                                    <li>
+                                                        Non-smoking facility
+                                                    </li>
+                                                </ul>
+                                            </section>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/bXq6MdJETZkkn5NxhcjgCg/o.jpg" className="img-fluid article-image"></img>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="alt-1"> 
+                                <div class="row mt-3 article">
+                                    <div class="col-md mr-3">
+                                    <img src="https://s3-media0.fl.yelpcdn.com/bphoto/vjzzQCs8c2CCVrgXdOVoZA/o.jpg" className="img-fluid article-image"></img>
+                                    </div>
+                                    <div class="col">
+                                        <div class="right-div">
+                                        <section>
+                                                <p>Room Features:</p>
+                                                <ul className="mt-n3" aria-label="">
+                                                    <li>
+                                                    Air conditioning
+                                                    </li>
+                                                    <li>
+                                                    Room service-There is room service available to resort guests, which offers continental breakfast (7-11 a.m.) and light (sandwiches, pizza, snacks) dining throughout the day (11 a.m.-11 p.m.).
+                                                    </li>
+                                                    <li>
+                                                    Refrigerator
+                                                    </li>
+                                                    <li>
+                                                    Housekeeping
+                                                    </li>
+                                                    <li>
+                                                    Safe 
+                                                    </li>
+                                                    <li>
+                                                    Flatscreen TV
+                                                    </li>
+                                                    <li>
+                                                    Average guest room is 314 square feet
+                                                    </li>
+                                                    <li>
+                                                    Rooms only have one sink
+                                                    </li>
+                                                    <li>
+                                                    Sliding wood doors across sink area 
+                                                    </li>
+                                                    <li>
+                                                    4-cup coffee makers (decaf and regular coffee packets) 
+                                                    </li>
+                                                    <li>
+                                                    In-room iron/ironing board
+                                                    </li>
+                                                    <li>
+                                                    In-room hair dryer
+                                                    </li>
+                                                </ul>
+                                            </section>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr></hr>
+                            <div className="row">
+                                <div className="col">
+                                    <Card>
+                                        <Card.Img variant="top" className="card-top" src="https://s3-media0.fl.yelpcdn.com/bphoto/bXq6MdJETZkkn5NxhcjgCg/o.jpg" />
+                                        <Card.Body>
+                                            <Card.Title>Food</Card.Title>
+                                            <Card.Text>
+                                                Enjoy some amazing food from a variety of restaurants within Disney’s Coronado Springs Resort!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                                <div className="col">
+                                    <Card>
+                                        <Card.Img variant="top" className="card-top" src="https://s3-media0.fl.yelpcdn.com/bphoto/vjzzQCs8c2CCVrgXdOVoZA/o.jpg" />
+                                        <Card.Body>
+                                            <Card.Title>Recreation</Card.Title>
+                                            <Card.Text>
+                                                Let yourself live out the wonderful recreation options in Disney’s Coronado Springs Resort!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            </div>
                         </div>
                         :null
                     }
