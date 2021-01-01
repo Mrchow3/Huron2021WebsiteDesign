0984c504c2ff25daf54c1943c9c627ed2ac106bb
pschwendy <peterschwende@gmail.com>
2021-01-01 11:42:59 -0500

    added yacht club to hotels page, remind me add addresses

--- huron/src/components/pages/Hotels.js
+++ huron/src/components/pages/Hotels.js
@@ -43,19 +43,19 @@ class Hotels extends Component{
     constructor() {
         super();
         this.state = {
-            yacht: this.HOTEL.YACHT,
+            hotel: this.HOTEL.YACHT,
         };
     }
 
     switchHotelsToSprings() {
         this.setState({
-            yacht: this.HOTEL.SPRINGS
+            hotel: this.HOTEL.SPRINGS
         });
     }
 
     switchHotelsToYacht() {
         this.setState({
-            yacht: this.HOTEL.YACHT
+            hotel: this.HOTEL.YACHT
         });
     }
 
@@ -77,16 +77,16 @@ class Hotels extends Component{
                             <button className="btn no-click">
                                 Hotel: 
                             </button>
-                            <button id="yacht" className={this.state.yacht === this.HOTEL.YACHT ? "btn-primary btn" : "btn"} onClick={() => this.switchHotelsToYacht()}>
+                            <button id="yacht" className={this.state.hotel === this.HOTEL.YACHT ? "btn-primary btn" : "btn"} onClick={() => this.switchHotelsToYacht()}>
                                 Yacht Club
                             </button>
-                            <button className={this.state.yacht === this.HOTEL.SPRINGS ? "btn-primary btn ml-2" : "btn ml-2"} onClick={() => this.switchHotelsToSprings()}>
+                            <button className={this.state.hotel === this.HOTEL.SPRINGS ? "btn-primary btn ml-2" : "btn ml-2"} onClick={() => this.switchHotelsToSprings()}>
                                 Coronado Springs
                             </button>
                         </div>
                     </div>
                     {
-                        this.state.yacht === this.HOTEL.SPRINGS?
+                        this.state.hotel === this.HOTEL.SPRINGS?
                         <div>
                             <h2 className="heading">Disney’s Coronado Springs Resort</h2>
                             <hr className="the-hr"></hr>
@@ -153,7 +153,7 @@ class Hotels extends Component{
                                         </div>
                                     </div>
                                     <div class="col">
-                                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/bXq6MdJETZkkn5NxhcjgCg/o.jpg" className="img-fluid article-image"></img>
+                                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/bXq6MdJETZkkn5NxhcjgCg/o.jpg" className="cropped-400 img-fluid article-image"></img>
                                     </div>
                                 </div>
                             </div>
@@ -235,7 +235,169 @@ class Hotels extends Component{
                                 </div>
                             </div>
                         </div>
-                        :null
+                        :
+                        <div>
+                            <h2 className="heading">Disney’s Yacht Club Resort</h2>
+                            <hr className="the-hr"></hr>
+                            <div class="row mt-3 article">
+                                <div class="col MR-3">
+                                    <div class="right-div">
+                                        <p class="left-justified">Disney’s Yacht Club Resort is built in a New England-style yacht club style and is located on Crescent Lake, right next door to its sister hotel, the Beach Club Resort. Each room and lobby is decorated with nautical touches to complete the seafaring experience. With over ten dining locations in this hotel, it is the perfect place for anyone looking to switch up from their daily life. Renting watercraft from Bayside Marina is a fun way to spend a sunny day, and boat rides are available for travelling to Disney’s Hollywood Studios nearby. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr></hr>
+                            <div class="alt-1"> 
+                                <div className="row article">
+                                    <div class="col-md mr-3">
+                                        <div class="right-div">
+                                            <section>
+                                                <p>General Amenities/Information:</p>
+                                                <ul className="mt-n3" aria-label="">
+                                                    <li>
+                                                    Built 11/5/1990
+                                                    </li>
+                                                    <li>
+                                                    630 rooms total, 539 regular guest rooms
+                                                    </li>
+                                                    <li>
+                                                    ADA rooms: 10 
+                                                    </li>
+                                                    <li>
+                                                    Storages
+                                                    </li>
+                                                    <li>
+                                                    Free WIFI 
+                                                    </li>
+                                                    <li>
+                                                    Free airport transportation
+                                                    </li>
+                                                    <li>
+                                                    Self-parking - $25 per night, valet parking available for $33
+                                                    </li>
+                                                    <li>
+                                                    Breakfast buffet
+                                                    </li>
+                                                    <li>
+                                                    Hot tub 
+                                                    </li>
+                                                    <li>
+                                                    24-hour security 
+                                                    </li>
+                                                    <li>
+                                                    24-hour front desk
+                                                    </li>
+                                                    <li>
+                                                    Concierge
+                                                    </li>
+                                                    <li>
+                                                    Spa
+                                                    </li>
+                                                    <li>
+                                                    Laundry and dry cleaning services
+                                                    </li>
+                                                    <li>
+                                                    Fitness center
+                                                    </li>
+                                                    <li>
+                                                    Non-smoking hotel
+                                                    </li>
+                                                    <li>
+                                                    Free shuttle or taxi service
+                                                    </li>
+                                                    <li>
+                                                    Outdoor pool
+                                                    </li>
+                                                    <li>
+                                                    Lazy river
+                                                    </li>
+                                                    <li>
+                                                    Heated Pool
+                                                    </li>
+                                                </ul>
+                                            </section>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <img src="https://mickeyblog.com/wp-content/uploads/2018/08/Yacht-Club-720x340.jpg" className="img-fluid article-image cropped-500"></img>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="alt-1"> 
+                                <div class="row mt-3 article">
+                                    <div class="col-md mr-3">
+                                    <img src="https://mickeyblog.com/wp-content/uploads/2018/07/Beach-Club-Pool-Disney.jpg" className="img-fluid article-image"></img>
+                                    </div>
+                                    <div class="col">
+                                        <div class="right-div">
+                                        <section>
+                                                <p>Room Features:</p>
+                                                <ul className="mt-n3" aria-label="">
+                                                    <li>
+                                                    Air conditioning
+                                                    </li>
+                                                    <li>
+                                                    Room service
+                                                    </li>
+                                                    <li>
+                                                    Refrigerator
+                                                    </li>
+                                                    <li>
+                                                    Housekeeping
+                                                    </li>
+                                                    <li>
+                                                    Safe 
+                                                    </li>
+                                                    <li>
+                                                    Flatscreen TV
+                                                    </li>
+                                                    <li>
+                                                    Private balcony
+                                                    </li>
+                                                    <li>
+                                                    Iron
+                                                    </li>
+                                                    <li>
+                                                    Desk
+                                                    </li>
+                                                    <li>
+                                                    Hair dryer 
+                                                    </li>
+                                                    <li>
+                                                    Double-sinks
+                                                    </li>
+                                                </ul>
+                                            </section>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr></hr>
+                            <div className="row">
+                                <div className="col">
+                                    <Card>
+                                        <Card.Img variant="top" className="card-top" src="https://s3-media0.fl.yelpcdn.com/bphoto/Qq9LDQx6PkaHeS74GQxHqw/o.jpg" />
+                                        <Card.Body>
+                                            <Card.Title>Food</Card.Title>
+                                            <Card.Text>
+                                                Enjoy some amazing food from a variety of restaurants within Disney’s Yacht Club Resort!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                                <div className="col">
+                                    <Card>
+                                        <Card.Img variant="top" className="card-top" src="http://4.bp.blogspot.com/_zSWJkb_7Edw/S7RTARKUIoI/AAAAAAAAAms/_LzIq6HzAjE/s1600/Stormalong+Bay.JPG" />
+                                        <Card.Body>
+                                            <Card.Title>Recreation</Card.Title>
+                                            <Card.Text>
+                                                Let yourself live out the wonderful recreation options in Disney’s Yacht Club Resort!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            </div>
+                        </div>
                     }
                 </div>
             </div>
