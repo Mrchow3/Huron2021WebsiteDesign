c198a9db0450f38f8e7f014296f6e62bf52e75dc
pschwendy <peterschwende@gmail.com>
2021-01-02 18:48:57 -0500

    phone changes, should work now

--- huron/src/components/inputs/HomeInputs.js
+++ huron/src/components/inputs/HomeInputs.js
@@ -71,8 +71,8 @@ carouselInputs.forEach((element) =>
   carouseloutputs.push(
     <Carousel.Item>
       <div className="head-hero">
-          <div className="row d-lg-block">
-              <img className="d-block img-thumbnail d-block w-100 carousel-image" alt={element.title} src={element.image} />
+          <div className="">
+              <img className="img-fluid d-block w-100 carousel-image" alt={element.title} src={element.image} />
           </div>
         <div className="carousel-heading bg-transparent overflow-hidden py-4 py-md-5 h-100">
           <div class="container-fluid mx-auto py-4 py-md-5">
