d5ae084b95cbb29046c815c8211869e4882bccda
pschwendy <peterschwende@gmail.com>
2021-01-03 16:49:32 -0500

    added sightseeing pages

--- huron/src/App.js
+++ huron/src/App.js
@@ -70,13 +70,85 @@ function App() {
         />} />
         <Route path='/explore-sightseeing' component={ExSightseeing} />
         <Route path='/explore-sightseeing-harry-p-leu-gardens' component={ExSightseeingSingle} children={ 
-        <ExSightseeingSingle
-          title = {sightseeingInputs[0].title}
-          blurb = {sightseeingInputs[0].blurb}
-          website = {sightseeingInputs[0].website}
-          prices = {sightseeingInputs[0].prices}
-          hours = {sightseeingInputs[0].hours}
-          image = {sightseeingInputs[0].image}
+          <ExSightseeingSingle
+            title = {sightseeingInputs[0].title}
+            blurb = {sightseeingInputs[0].blurb}
+            website = {sightseeingInputs[0].website}
+            prices = {sightseeingInputs[0].prices}
+            hours = {sightseeingInputs[0].hours}
+            image = {sightseeingInputs[0].image}
+        />} />
+        <Route path='/explore-sightseeing-florida-tours' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[1].title}
+            blurb = {sightseeingInputs[1].blurb}
+            website = {sightseeingInputs[1].website}
+            prices = {sightseeingInputs[1].prices}
+            hours = {sightseeingInputs[1].hours}
+            image = {sightseeingInputs[1].image}
+        />} />
+        <Route path='/explore-sightseeing-tibet-butler-preserve' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[2].title}
+            blurb = {sightseeingInputs[2].blurb}
+            website = {sightseeingInputs[2].website}
+            prices = {sightseeingInputs[2].prices}
+            hours = {sightseeingInputs[2].hours}
+            image = {sightseeingInputs[2].image}
+        />} />
+        <Route path='/explore-sightseeing-lake-eola-park' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[3].title}
+            blurb = {sightseeingInputs[3].blurb}
+            website = {sightseeingInputs[3].website}
+            prices = {sightseeingInputs[3].prices}
+            hours = {sightseeingInputs[3].hours}
+            image = {sightseeingInputs[3].image}
+        />} />
+        <Route path='/explore-sightseeing-exotic-animals' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[4].title}
+            blurb = {sightseeingInputs[4].blurb}
+            website = {sightseeingInputs[4].website}
+            prices = {sightseeingInputs[4].prices}
+            hours = {sightseeingInputs[4].hours}
+            image = {sightseeingInputs[4].image}
+        />} />
+        <Route path='/explore-sightseeing-butler-chain-lakes' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[5].title}
+            blurb = {sightseeingInputs[5].blurb}
+            website = {sightseeingInputs[5].website}
+            prices = {sightseeingInputs[5].prices}
+            hours = {sightseeingInputs[5].hours}
+            image = {sightseeingInputs[5].image}
+        />} />
+        <Route path='/explore-sightseeing-kissimmee-swamp-tours' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[6].title}
+            blurb = {sightseeingInputs[6].blurb}
+            website = {sightseeingInputs[6].website}
+            prices = {sightseeingInputs[6].prices}
+            hours = {sightseeingInputs[6].hours}
+            image = {sightseeingInputs[6].image}
+        />} />
+        <Route path='/explore-sightseeing-winter-park-boat-tour' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[7].title}
+            blurb = {sightseeingInputs[7].blurb}
+            website = {sightseeingInputs[7].website}
+            prices = {sightseeingInputs[7].prices}
+            hours = {sightseeingInputs[7].hours}
+            image = {sightseeingInputs[7].image}
+        />} />
+        <Route path='/explore-sightseeing-wildlife-attractions' component={ExSightseeingSingle} children={ 
+          <ExSightseeingSingle
+            title = {sightseeingInputs[8].title}
+            blurb = {sightseeingInputs[8].blurb}
+            website = {sightseeingInputs[8].website}
+            prices = {sightseeingInputs[8].prices}
+            hours = {sightseeingInputs[8].hours}
+            image = {sightseeingInputs[8].image}
         />} />
         <Route path='/hotels' component={Hotels} />
       </Switch>
